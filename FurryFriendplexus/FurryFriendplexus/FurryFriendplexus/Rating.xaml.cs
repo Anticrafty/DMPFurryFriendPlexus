d6ebae17239a00dfb2dc3d26dfd05b9f6c1a4e00
Anticrafty <piratnovak@seznam.cz>
2020-02-02 18:26:02 +0100

    Integer snapping on Rating Slider

--- FurryFriendplexus/FurryFriendplexus/FurryFriendplexus/Rating.xaml.cs
+++ FurryFriendplexus/FurryFriendplexus/FurryFriendplexus/Rating.xaml.cs
@@ -16,5 +16,14 @@ namespace FurryFriendplexus
         {
             InitializeComponent();
         }
+
+        // Snapping slider on integers
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var newStep = Math.Round(e.NewValue / 1.0);
+
+            RatingSlider.Value = newStep * 1.0;
+            // https://forums.xamarin.com/discussion/22473/can-you-limit-a-slider-to-only-allow-integer-values-hopefully-snapping-to-the-next-integer
+        }
     }
 }
